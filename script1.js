7a3d49182049d59cbd55a096cbc79a138837853a
Ramon Wong <rcnwong78@gmail.com>
2022-08-31 18:02:52 -0300

    I rather simplifly the FormMenu then let it read json files for configurations

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 var Pages       = ["button1", "button2", "button3", "button4", "button5"];
-var Functions   = [funct1, funct2, funct3, funct4, BuildForm];
+var Functions   = [funct1, funct2, funct3, funct4, funct5];
 var content     = document.getElementById("content").getElementsByClassName("wrap")[0];
 
 // convert javascript string into a JSON object
@@ -7,11 +7,19 @@ var content     = document.getElementById("content").getElementsByClassName("wra
 var key     = sessionStorage.getItem("key");
 // create javascript json with the key and the value
 
+var Buildform_str = '{"name": "Test Form", "method": "post", "button":"Submit", "Layout": [ 0, 1, 0, 2, 0]}';
+var Buildform_json = JSON.parse(Buildform_str);
+
+fMenu   = new FormMenu();
 function funct1(){      RequestPage("listener.php", JSON.parse('{"key": "' + key + '", "type": "page", "page": 1}'), content);}
 function funct2(){      RequestPage("listener.php", JSON.parse('{"key": "' + key + '", "type": "page", "page": 2}'), content);}
 function funct3(){      RequestPage("listener.php", JSON.parse('{"key": "' + key + '", "type": "page", "page": 3}'), content);}
 function funct4(){      RequestPage("listener.php", JSON.parse('{"key": "' + key + '", "type": "page", "page": 4}'), content);}
-//function funct5(){      RequestPage("listener.php", JSON.parse('{"key": "' + key + '", "type": "page", "page": "login"}'), content);}
+function funct5(){      
+    console.log("funct 5"); 
+
+    fMenu.BuildForm( Buildform_json);
+}
 
 for(let x in Pages){
     var Element = document.getElementById(Pages[x]);
--- script3.js
+++ script3.js
@@ -1,17 +1,22 @@
 
 
 
-function BuildForm(){
-    function AddBr(){
-        return( document.createElement("br"));
-    }
 
-    function AddLbl(name){
+function FormMenu(){
+    this.form = document.createElement("form");
+
+    this.form.setAttribute("name", "login_forms");
+    this.form.setAttribute("method", "post"); 
+
+    let AddBr = ()=>{ return( document.createElement("br")); }
+
+    function AddLbl(name){ 
         var lbl    = document.createElement("label");
         lbl.setAttribute("for",name);
         lbl.innerHTML  = name;
         return lbl;
     }
+
 
     function AddInputText(type, id, name){
         var input   = document.createElement("input");
@@ -21,43 +26,99 @@ function BuildForm(){
         input.setAttribute("name", name);
 
         return input;
-    }
+    }    
 
-    function AddInputSubmit( name){
+    function AddSubmitButton( name, value){ 
         var input   = document.createElement("input");
 
-        input.setAttribute("type", "submit");
-        input.setAttribute("value", "submit");
+        input.setAttribute("type", name);
+        input.setAttribute("value", value);
 
         return input;
     }
 
-    var form = document.createElement("form");
-    form.setAttribute("name", "login_forms");
-    form.setAttribute("method", "post");
+    this.BuildForm = function( layout_form){
 
-    form.appendChild(AddLbl("useremail"));
-    form.appendChild(AddBr());
-    form.appendChild(AddInputText("text", "email", "email"));
-    form.appendChild(AddBr());
-    form.appendChild(AddLbl("Password"));
-    form.appendChild(AddBr());
-    form.appendChild(AddInputText("password", "pass", "pass"));
-    form.appendChild(AddBr());
-    form.appendChild(AddBr());
-    form.appendChild(AddInputSubmit("submit"));
+        this.form.appendChild( AddLbl("useremail"));
+        this.form.appendChild( AddBr());
+        this.form.appendChild( AddInputText("text", "email", "email"));
+        this.form.appendChild( AddBr());
+        this.form.appendChild( AddLbl("Password"));
+        this.form.appendChild( AddBr());
+        this.form.appendChild( AddInputText("password", "pass", "pass"));
+        this.form.appendChild( AddBr());
+        this.form.appendChild( AddBr());
+        this.form.appendChild( AddSubmitButton("submit"));
 
-    content.innerHTML = "";
-    content.appendChild(form);
-
-    form.addEventListener('submit', function(evt){
-        evt.preventDefault();
-
-        if( ValidateEmail(form.email) == true){
-            var email = form.email.value;
-            var pass  = form.pass.value;
-            var jsonstr = '{"key": "' + key + '", "page": "request", "email": "' + email + '", "pass": "' + pass + '"}';
-            RequestPage("listener.php", JSON.parse(jsonstr), content);
-        }
-    });    
+        content.innerHTML = "";
+        content.appendChild(this.form);
+    }        
 }
+
+
+
+
+
+
+// function BuildForm(){
+//     function AddBr(){
+//         return( document.createElement("br"));
+//     }
+
+//     function AddLbl(name){
+//         var lbl    = document.createElement("label");
+//         lbl.setAttribute("for",name);
+//         lbl.innerHTML  = name;
+//         return lbl;
+//     }
+
+//     function AddInputText(type, id, name){
+//         var input   = document.createElement("input");
+
+//         input.setAttribute("type", type);
+//         input.setAttribute("id", id);
+//         input.setAttribute("name", name);
+
+//         return input;
+//     }
+
+//     function AddInputSubmit( name){
+//         var input   = document.createElement("input");
+
+//         input.setAttribute("type", "submit");
+//         input.setAttribute("value", "submit");
+
+//         return input;
+//     }
+
+//     var form = document.createElement("form");
+//     form.setAttribute("name", "login_forms");
+//     form.setAttribute("method", "post");
+
+//     form.appendChild(AddLbl("useremail"));
+//     form.appendChild(AddBr());
+//     form.appendChild(AddInputText("text", "email", "email"));
+//     form.appendChild(AddBr());
+//     form.appendChild(AddLbl("Password"));
+//     form.appendChild(AddBr());
+//     form.appendChild(AddInputText("password", "pass", "pass"));
+//     form.appendChild(AddBr());
+//     form.appendChild(AddBr());
+//     form.appendChild(AddInputSubmit("submit"));
+
+//     content.innerHTML = "";
+//     content.appendChild(form);
+
+//     form.addEventListener('submit', function(evt){
+//         evt.preventDefault();
+
+//         if( ValidateEmail(form.email) == true){
+//             var email = form.email.value;
+//             var pass  = form.pass.value;
+//             var jsonstr = '{"key": "' + key + '", "page": "request", "email": "' + email + '", "pass": "' + pass + '"}';
+//             RequestPage("listener.php", JSON.parse(jsonstr), content);
+//         }
+//     });    
+// }
+
+
