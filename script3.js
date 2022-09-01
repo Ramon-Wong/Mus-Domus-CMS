195763f4b4c633d45dcd28e30d37ad49fd55d007
Ramon Wong <rcnwong78@gmail.com>
2022-08-31 22:11:38 -0300

    clean up at script3.js

--- script3.js
+++ script3.js
@@ -16,19 +16,6 @@ function FormMenu(){
         return lbl;
     }
 
-
-    //       AddInputText("text", "email", "email"));
-    function AddInputText(type, id, name){
-        var input   = document.createElement("input");
-
-        input.setAttribute("type", type);
-        input.setAttribute("id", id);
-        input.setAttribute("name", name);
-
-        return input;
-    }    
-
-
     function _AddInput( type, id, name, value){
         var input   = document.createElement("input");
 
@@ -40,29 +27,6 @@ function FormMenu(){
         return input;
     }
 
-
-    //       AddSubmitButton("submit"));
-    function AddSubmitButton( name, value){ 
-        var input   = document.createElement("input");
-
-        input.setAttribute("type", name);
-        input.setAttribute("value", value);
-        input.setAttribute("id", "");
-
-        return input;
-    }
-
-
-    function AddInput(type, id, name){
-        var input   = document.createElement("input");
-
-        input.setAttribute("type", type);
-        input.setAttribute("id", id);
-        input.setAttribute("name", name);
-
-        return input;
-    }    
-
     //button:   type = button,      id = id,        name = name,    value = "" 
     //input1:   type = text,        id = id,        name = name,    value = ""   
     //input2:   type = password,    id = "pass",    name = "pass",  value = ""
@@ -71,18 +35,17 @@ function FormMenu(){
 
         this.form.appendChild( AddLbl("useremail"));
         this.form.appendChild( AddBr());
-        // this.form.appendChild( AddInputText("text", "email", "email"));
-        this.form.appendChild( _AddInput( "text", "email", "email", ""));        // type, id, name, value             email
+        this.form.appendChild( _AddInput( "text", "email", "email", ""));               // type, id, name, value             email
 
         this.form.appendChild( AddBr());
         this.form.appendChild( AddLbl("Password"));
+
         this.form.appendChild( AddBr());
-        // this.form.appendChild( AddInputText("password", "pass", "pass"));
-        this.form.appendChild( _AddInput( "password", "pass", "pass", ""));           // type, id, name, value             
+        this.form.appendChild( _AddInput( "password", "pass", "pass", ""));             // type, id, name, value             
+
         this.form.appendChild( AddBr());
         this.form.appendChild( AddBr());
-        // this.form.appendChild( AddSubmitButton("submit"));
-        this.form.appendChild( _AddInput( "button", "", "name", "submit"));           // type, id, name, value         Button
+        this.form.appendChild( _AddInput( "button", "", "name", "submit"));             // type, id, name, value         Button
 
         content.innerHTML = "";
         content.appendChild(this.form);
