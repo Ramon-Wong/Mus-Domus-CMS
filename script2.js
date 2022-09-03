f9a58f3c4807971cf81c1b865b4037de777992f2
Ramon Wong <rcnwong78@gmail.com>
2022-09-02 23:53:55 -0300

    added encryption functions

--- script2.js
+++ script2.js
@@ -90,3 +90,27 @@ function ValidateEmail(input) {
 
     return result;
 }
+
+
+var CryptoJSAesJson = {
+    stringify: function(cipherParams){
+        var j = {ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64)};
+        if (cipherParams.iv) j.iv = cipherParams.iv.toString();
+        if (cipherParams.salt) j.s = cipherParams.salt.toString();
+
+        return JSON.stringify(j);
+    },
+    parse: function(jsonStr) {
+        var j = JSON.parse(jsonStr);
+        var cipherParams = CryptoJS.lib.CipherParams.create({ciphertext: CryptoJS.enc.Base64.parse(j.ct)});
+        if (j.iv) cipherParams.iv = CryptoJS.enc.Hex.parse(j.iv)
+        if (j.s) cipherParams.salt = CryptoJS.enc.Hex.parse(j.s)
+
+        return cipherParams;
+    }
+}
+
+// usage
+// var encrypted = CryptoJS.AES.encrypt(JSON.stringify("value to encrypt"), "my passphrase", {format: CryptoJSAesJson}).toString();
+// var decrypted = JSON.parse(CryptoJS.AES.decrypt(encrypted, "my passphrase", {format: CryptoJSAesJson}).toString(CryptoJS.enc.Utf8));
+
