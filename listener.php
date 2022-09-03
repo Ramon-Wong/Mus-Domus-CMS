865b2fdf6b0f387be1157600e0f822583f932abe
Ramon Wong <rcnwong78@gmail.com>
2022-09-02 22:28:19 -0300

    added some to do stuff in listener also need to create a proper split in branches for this

--- listener.php
+++ listener.php
@@ -7,4 +7,30 @@ $obj            = json_decode($requestPayload);
 
 echo json_encode($obj);
 
+
+switch($obj->type){
+    case "page":
+        // stuff to do with a page
+        break;
+    case "login":
+        // stuff to do with a login
+        break;
+    case "logout":
+        // stuff to do with a logout
+        break;    
+
+    case "test":
+        // stuff to do with a test
+        break;
+
+    case "nav":
+        // stuff to do with a nav
+        break;
+    default:
+
+        break;
+}
+
+
+
 ?>
